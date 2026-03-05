fec3157c579c32310ed07943e90365f7a4e6a7c3
inscript <inscript2023@gmail.com>
2026-03-05 20:31:38 +0900

    refactor: 작가 레이아웃 코드 포맷팅 정리

--- app/(author)/layout.tsx
+++ app/(author)/layout.tsx
@@ -7,6 +7,7 @@ interface AuthorLayoutProps {
 
 export default function AuthorLayout({ children }: AuthorLayoutProps) {
   return (
+    
     <main className="flex min-h-screen flex-col">
       <Header />
       <div className="mx-auto w-full max-w-[1440px] flex-1 flex-col">
